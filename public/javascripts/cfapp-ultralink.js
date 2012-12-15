6c2ce7293c6a84e9b37199d02de2e5e0ca9e9336
Spencer Nielsen <snielsen@aorensoftware.com>
2012-12-15 12:33:48 -0800

    Initial commit.

---
+++ public/javascripts/cfapp-ultralink.js
@@ -0,0 +1,6 @@
+CloudFlare.define("cfapp_ultralink", ["cfapp_ultralink/config", "https://ultralink.me/ultralink.js"], function( config, ultralink )
+{
+    // config.domain_id
+
+    ultralink.startUltralink();
+} );
